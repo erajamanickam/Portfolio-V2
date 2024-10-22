888090422c7beae8c4e96992f1f157e40049cc26
rajamanickam <erajamanickam72@gmail.com>
2024-10-22 21:18:16 +0530

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = ({ data }) => {
     return (
         <footer className="text-white py-4 shadow-[0_0_3px_rgba(255,255,255,0.15)] bg-slate-900 z-20 relative">
             <div className="text-center md:text-left container mx-auto flex flex-col md:flex-row md:justify-between">
-                <div className="text-sm mb-2 md:mb-0 flex items-center"><FaRegCopyright className="mr-1" /> {currentYear}. {footerItems?.section_body?.copyright}</div>
+                <div className="text-sm mb-2 md:mb-0 flex justify-center items-center"><FaRegCopyright className="mr-1" /> {currentYear}. {footerItems?.section_body?.copyright}</div>
                 <div className="text-sm">{footerItems?.section_body?.developed}</div>
             </div>
         </footer>
