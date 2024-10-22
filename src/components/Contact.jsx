dced4c60bb63441d3879715dff298e678b00792d
rajamanickam <erajamanickam72@gmail.com>
2024-10-22 20:43:38 +0530

    env file secure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ import { FaLinkedin } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 
-
+ 
 const Contact = ({ data }) => {
     const [contactItems, setContactItems] = useState([]);
 
